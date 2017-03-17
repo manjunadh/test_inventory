39ef6017c34463c8b3a8da10bd1041a6d1e13bdb
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-17 16:23:24 +0530

    test

--- core/composite-service/src/main/java/com/fgl/microservices/composite/Application.java
+++ core/composite-service/src/main/java/com/fgl/microservices/composite/Application.java
@@ -4,18 +4,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.ComponentScan;
 
-import zipkin.Span;
-
 @SpringBootApplication
-@EnableAspectJAutoProxy
 @EnableDiscoveryClient
 @ComponentScan({"com.fgl.microservices.composite.services", "com.fgl.microservices.util"})
 public class Application {
@@ -25,23 +17,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
-	@Bean
-	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
-	public ZipkinSpanReporter spanCollector() {
-		return new ZipkinSpanReporter() {
-
-			@Override
-			public void report(Span span) {
-
-				log.info(String.format("Reporting span [%s]", span));
-			}
-		};
-	}
+	
 
 	
 	
